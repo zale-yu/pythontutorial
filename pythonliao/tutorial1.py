8f8b19bfcc13ce39968c3e7f34c6f71ad5e245fb
zale <1902298957@qq.com>
2021-07-04 17:07:25 +0800

    input and output

---
+++ pythonliao/tutorial1.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# print('hello world')
+# print('the quick brown fox','jumps over','the lazy dog')
+# print(300)
+# print(100+200)
+# print('100 + 200 =',100 + 200)
+#
+
+# # input
+# name = input()
+#
+# print(name)
+
+name = input('please enter your name: ')
+print('hello',name)
+
