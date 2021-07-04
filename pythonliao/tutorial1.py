8aae1488bb994478e37ed5507bf1ac402a7f505e
zale <1902298957@qq.com>
2021-07-05 07:03:13 +0800

    list function

--- pythonliao/tutorial1.py
+++ pythonliao/tutorial1.py
@@ -3,6 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+
+'''
 # print('hello world')
 # print('the quick brown fox','jumps over','the lazy dog')
 # print(300)
@@ -17,4 +19,74 @@
 
 name = input('please enter your name: ')
 print('hello',name)
+'''
+
+
+# # 转义字符
+# print('i\'m ok.')
+# 
+# print('i \'m learning \n python.')
+# 
+# print('\\\n\\')
+# 
+# # r'' 表示''内部字符不转译
+# 
+# print('\\\t\\')
+# 
+# print(r'\\\t\\')
+#
+# # '''...''' 可以表示多行内容
+# 
+# print('''line1
+# ... line2
+# ... line3
+#
+#
+# )
+#
+# print('''
+#      line1
+#      line2
+#      line3''')
+
+
+# list
+#
+# classmates = ['Michael','bob','tracy']      #一个list
+# print(classmates)
+#
+# print(len(classmates))                            #len 用来查看list 元素个数
+#
+# print(classmates[0])                              #计数从0开始，-1可以表示最后一个数
+# print(classmates[-1])
+# print(classmates[-2])
+#
+# #在list 中追加元素到末尾
+# classmates.append('Adam')
+# print(classmates)
+#
+# #把元素插入指定位置
+# classmates.insert(1,'jack')
+# print(classmates)
+#
+# #删除list末尾元素，用pop（）方法；
+# classmates.pop()
+#
+# #删除list指定位置元素，用pop（i）方法；
+# classmates.pop(1)
+#
+#
+# #替换元素
+# classmates[1] = 'sarah'
+#
+# #list 中元素类型可以不同
+# L = ['apple',123,True]
+#
+# #list可以是另一个list
+# s = ['python','java',['asp','php'],'scheme']
+# len(s)                  #个数为4
+#
+# # S 可拆分为两个list
+# p = ['asp','php']
+# s = ['python','java', p,'scheme']       #要拿到'php'可以写成p[1]或者s[2][1]  s可以看成是一个二维数组，类似的还有三维、四维……数组
 
